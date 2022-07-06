6806f853f5c2ddace74bf129a85228ecc5daf29c
dduartevla <deboraduarte@ice.ufjf.br>
2022-07-05 23:17:08 -0300

    Cria layout para Adicionar nova espécie.

---
+++ app/src/main/java/br/ufjf/dcc196/izabel/avistamentos/AdicionaEspecie.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc196.izabel.avistamentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionaEspecie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adiciona_especie);
+    }
+}
